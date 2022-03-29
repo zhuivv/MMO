8b8a994c6261c13aadc3d930ce81586e543663f0
zhuivv <96205857+zhuivv@users.noreply.github.com>
2022-03-29 17:15:15 -0400

    Update permutation.m

--- multimedia/permutation.m
+++ multimedia/permutation.m
@@ -14,10 +14,10 @@ t0 = abs(sum(-log(pmat(M_in,Y_in)),[1 2])/sqrt(length(M_in)*length(Y_in)));
 permt=[];
 parfor i=1:B
 Mind = datasample(1:size(M,1),size(M,1));
-xind = datasample(1:length(x),length(x));
+Yind = datasample(1:size(Y,1),size(Y,1));
 
-M_perm = M(Mind,:); x_perm = x(xind);
-[M_pin Y_pin effp pmatp] = M6(x_perm,M_perm,Y);
+M_perm = M(Mind,:); Y_perm = Y(Yind,:);
+[M_pin Y_pin effp pmatp] = M6(x,M_perm,Y_perm);
 permt(i) = abs(sum(-log(pmatp(M_pin,Y_pin)),[1 2])/sqrt(length(M_pin)*length(Y_pin)));
 
 end
